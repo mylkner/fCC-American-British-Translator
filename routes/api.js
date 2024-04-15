17810be27a0079a0bee79e015c5e68cc03b76095
mylkner <mylofaulkner27@gmail.com>
2024-04-15 03:06:39 +0200

    route functionality

--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,36 @@
-'use strict';
+"use strict";
 
-const Translator = require('../components/translator.js');
+const Translator = require("../components/translator.js");
 
 module.exports = function (app) {
-  
-  const translator = new Translator();
+    const translator = new Translator();
 
-  app.route('/api/translate')
-    .post((req, res) => {
-      
+    app.route("/api/translate").post((req, res) => {
+        const text = req.body.text;
+        const locale = req.body.locale;
+
+        if (
+            !req.body.hasOwnProperty("text") ||
+            !req.body.hasOwnProperty("locale")
+        ) {
+            return res.json({ error: "Required field(s) missing" });
+        }
+
+        if (!text) {
+            return res.json({ error: "No text to translate" });
+        }
+
+        if (
+            locale !== "british-to-american" &&
+            locale !== "american-to-british"
+        ) {
+            return res.json({ error: "Invalid value for locale field" });
+        }
+
+        if (locale === "american-to-british") {
+            return res.json(translator.americanToBritish(text));
+        } else {
+            return res.json(translator.britishToAmerican(text));
+        }
     });
 };
